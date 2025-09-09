34e5eff50d59affd7304d136f8ab98d84b6ac220
ruisxx <ruixdxd69@gmail.com>
2025-09-09 20:18:31 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -2,9 +2,9 @@
 module.exports = {
   SESSION_ID: "",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "18186706095", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "Spain/gmt+2" //put your country timeZone....leave blank if u don't know.
 };
